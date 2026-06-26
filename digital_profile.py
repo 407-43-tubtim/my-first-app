042d6cc3d333dd73d355543574f23bb0a350a6a7
407-43-tubtim <56763@yupparaj.ac.th>
2026-06-26 11:52:23 +0700

    407-43-tubtim

--- digital_profile.py
+++ digital_profile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 1. หัวข้อใหญ่: แนะนำตัวหลักและชื่อเจ้าของเว็บ
- st.title("My Digital Profile")
+st.title("My Digital Profile")
 
 # 2. หัวข้อย่อยที่ 1: แนะนำตัว
 st.header("แนะนำตัว")
